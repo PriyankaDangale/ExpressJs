cef65dcc9f1a7289408336d655f6d0f7422a94bb
PriyankaDangale <prdangale2016@gmail.com>
2023-07-24 21:01:00 +0530

    success

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const http=require('http');
+const bodyparser=require('body-parser');
 const express=require('express');
 const app=express();
-app.use((req,res,next)=>{
+app.use(bodyparser.urlencoded({extended:false}));
+app.use('/addproduct',(req,res,next)=>{
     console.log("In the middleware");
-    next();
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"> <button type="submit">Add Product </form>');
+    
 })
-app.use((req,res,next)=>{
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
     console.log("In the other middleware");
     res.send('<h1>Helllo This is priyanka Dangale</h1>');
     
